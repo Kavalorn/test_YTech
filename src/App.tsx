fef6040708a9d78df0de02d8bc3bdca5a7c639fe
Kavalorn <kavalorn12345@gmail.com>
2021-03-04 19:44:53 +0200

    finally build an app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {
+    Container
+} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useRootState} from "./StateContext";
+import WordsCounter from "./components/WordsCounter";
+import WordsViewer from "./components/WordsViewer";
+import { css } from '@emotion/css';
 
 function App() {
-  return (
-    <div>test</div>
-  );
+    const rootState = useRootState();
+    const {dictionaryStore} = rootState;
+
+    return (
+        <Container className={css`
+                padding: 32px
+              `}
+        >
+            <WordsCounter descriptionText={"Get words count started with specific letter"} action={dictionaryStore.getWordsCountStartedWithLetter} />
+            <WordsCounter descriptionText={"Get words count with specific letter"} action={dictionaryStore.getWordsCountWithLetter} />
+            <WordsCounter descriptionText={"Get words count ended with specific letter"} action={dictionaryStore.getWordsCountEndedWithLetter} />
+            <WordsCounter descriptionText={"Get words count with specific letter repeated twice in a row"} action={dictionaryStore.getWordsCountWithSameLetterRepeatedTwice} />
+
+            <WordsViewer descriptionText={"Get word description"} action={dictionaryStore.getWordDescription} />
+            <WordsViewer descriptionText={"Get palindromes"} action={dictionaryStore.getPalindromes} withoutInput={true} />
+        </Container>
+    );
 }
 
 export default App;
